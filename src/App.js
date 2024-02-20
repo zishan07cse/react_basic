939665bc8615dab66695b04f10fbb3df47c16742
github_zishan07cse <zishan07csecuet@gmail.com>
2024-02-21 00:18:19 +0600

    Feat:first test app

--- src/App.js
+++ src/App.js
@@ -4,22 +4,21 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+     <Job salary={90000}  position="Senior Ios Developer" company="Amazon"/>
+           <Job salary={40000}  position="Node JS Developer" company="Facebook"/>
+
+           <Job salary={50000}  position="React JS Developer" company="Google"/>
+           </div>
   );
 }
-
+const Job = (props)=>{
+  return(
+    <div>
+      <h1>{props.salary}</h1>
+      <h1>{props.position}</h1>
+      <h1>{props.company}</h1>
+    </div>
+    
+    )
+}
 export default App;
